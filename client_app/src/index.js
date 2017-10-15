2f7bcbd540db55c0f1835a287adddf4dea8412de
tunguyennnnn <conghoa522@gmail.com>
2017-10-15 14:13:10 -0400

    react setup

---
+++ client_app/src/index.js
@@ -0,0 +1,18 @@
+import './styles/style.css'
+import React from 'react'
+import ReactDom from 'react-dom'
+import {Router, Route, IndexRoute, hashHistory, browserHistory} from 'react-router'
+import {syncHistoryWithStore} from 'react-router-redux'
+import {Provider} from 'react-redux'
+import MainPage from './containers/MainPage'
+import store from './Store'
+
+const app = document.getElementById('app')
+
+ReactDom.render(
+  <Provider store={store}>
+    <Router history={hashHistory}>
+      <Route path='/' component={MainPage}></Route>
+    </Router>
+  </Provider>
+, app)
