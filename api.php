b1f0b19e1a0ee28b0ea67a4daa0f71b16bead6a6
hyjaz <hyjaz.loudin@gmail.com>
2017-11-25 01:11:08 -0500

    remove php files

--- api.php
+++
@@ -1,6 +0,0 @@
-<?php
-// get the HTTP method, path and body of the request
-$method = $_SERVER['REQUEST_METHOD'];
-$request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
-echo $_SERVER['PATH_INFO'];
-?>
